ad017cd24b626136514842ef80b3b4a944a83c7e
Nick Cipollo <njc1115@gmail.com>
2018-09-20 09:13:02 -0400

    Move setup action into it's own file.

---
+++ action/setup.go
@@ -0,0 +1,54 @@
+package action
+
+import (
+	"io"
+	"github.com/ncipollo/fnew/config"
+	"github.com/ncipollo/fnew/repo"
+	"github.com/ncipollo/fnew/workspace"
+	"github.com/ncipollo/fnew/manifest"
+)
+
+func (action *SetupAction) Perform(output io.Writer) error {
+	err := action.workspace.Setup()
+	if err != nil {
+		return err
+	}
+	userConfig, err := config.FromFile(action.workspace.ConfigPath())
+	if err != nil {
+		return err
+	}
+	err = action.fetchConfigManifestIfNeeded(userConfig)
+	if err != nil {
+		return err
+	}
+
+	err = action.fetchDefaultManifestIfNeeded()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type SetupAction struct {
+	repo      repo.Repo
+	workspace workspace.Workspace
+}
+
+func (action *SetupAction) fetchConfigManifestIfNeeded(userConfig *config.Config) error {
+	if userConfig.ManifestRepoUrl == nil {
+		return nil
+	}
+	if !action.workspace.ConfigManifestRepoExists() {
+		_, err := action.repo.Clone(action.workspace.ConfigManifestRepoPath(), userConfig.ManifestRepoUrl.String())
+		return err
+	}
+	return nil
+}
+
+func (action *SetupAction) fetchDefaultManifestIfNeeded() error {
+	if !action.workspace.DefaultManifestRepoExists() {
+		_, err := action.repo.Clone(action.workspace.DefaultManifestRepoPath(), manifest.DefaultRepository)
+		return err
+	}
+	return nil
+}

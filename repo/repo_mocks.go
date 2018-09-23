ca4af8151d00c06318b09da3fa0c030f3d9d1f46
Nick Cipollo <njc1115@gmail.com>
2018-09-23 19:51:01 -0400

    Add repo mocking methods.

--- repo/repo_mocks.go
+++ repo/repo_mocks.go
@@ -3,10 +3,15 @@ package repo
 import (
 	"github.com/stretchr/testify/mock"
 	"gopkg.in/src-d/go-git.v4"
+	"errors"
 )
 
 type MockRepo struct {
 	mock.Mock
+}
+
+func newMockRepo() *MockRepo {
+	return &MockRepo{}
 }
 
 func (repo *MockRepo) Clone(localPath string, repoUrl string) (*git.Repository, error) {
@@ -24,5 +29,26 @@ func (repo *MockRepo) Pull(repository *git.Repository) error {
 	return args.Error(0)
 }
 
+func (repo *MockRepo) stubClone(shouldError bool)  {
+	if shouldError {
+		repo.On("Clone", mock.Anything, mock.Anything).Return(nil, errors.New("you're the clone"))
+	} else {
+		repo.On("Clone", mock.Anything, mock.Anything).Return(git.Repository{}, nil)
+	}
+}
 
+func (repo *MockRepo) stubOpen(shouldError bool)  {
+	if shouldError {
+		repo.On("Open", mock.Anything).Return(nil, errors.New("closed"))
+	} else {
+		repo.On("Open", mock.Anything).Return(git.Repository{}, nil)
+	}
+}
 
+func (repo *MockRepo) stubPull(shouldError bool)  {
+	if shouldError {
+		repo.On("Pull", mock.Anything).Return(errors.New("push"))
+	} else {
+		repo.On("Pull", mock.Anything).Return(nil)
+	}
+}

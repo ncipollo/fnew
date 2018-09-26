c40acf699c9dadf4079ae76dba8970d775a1bfd6
Nick Cipollo <njc1115@gmail.com>
2018-09-25 23:23:26 -0400

    Add MockMerger.

---
+++ merger/merger_fixture.go
@@ -0,0 +1,25 @@
+package merger
+
+import (
+	"github.com/stretchr/testify/mock"
+	"github.com/ncipollo/fnew/manifest"
+)
+
+type MockMerger struct{
+	mock.Mock
+}
+
+func NewMockManifest() *MockMerger {
+	mockMerger := MockMerger{}
+	mockMerger.On("MergedManifest").Return(manifest.FullManifest())
+	return &mockMerger
+}
+
+func (merger *MockMerger) MergedManifest() *manifest.Manifest {
+	args := merger.Called()
+
+	return args.Get(0).(*manifest.Manifest)
+}
+
+
+

28a1269db6448de3432d5f5320149b3cec1c5785
Nick Cipollo <njc1115@gmail.com>
2018-10-14 15:40:38 -0400

    Add transform options.

--- transform/transform.go
+++ transform/transform.go
@@ -9,3 +9,21 @@ const (
 	TransformTypeRunScript             Type = "run_script"
 	TransformTypeVariableStringReplace Type = "variable_string_replace"
 )
+
+type Transform interface {
+	Apply()
+}
+
+type TransformOptions struct {
+	Arguments []string `json:"arguments"`
+	InputPath *string `json:"input_path"`
+	InputVariable *string `json:"input_variable"`
+	OutputPath *string `json:"output_path"`
+	OutputVariable *string `json:"output_variable"`
+	StringReplace *StringReplace `json:"string_replace"`
+}
+
+type StringReplace struct {
+	Old string
+	New string
+}

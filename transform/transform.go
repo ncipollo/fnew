4f4327e2bb38682994ebed901e40e40956ba9e14
Nick Cipollo <njc1115@gmail.com>
2018-10-14 15:43:20 -0400

    Add type to transform options.

--- transform/transform.go
+++ transform/transform.go
@@ -15,12 +15,13 @@ type Transform interface {
 }
 
 type TransformOptions struct {
-	Arguments []string `json:"arguments"`
-	InputPath *string `json:"input_path"`
-	InputVariable *string `json:"input_variable"`
-	OutputPath *string `json:"output_path"`
-	OutputVariable *string `json:"output_variable"`
-	StringReplace *StringReplace `json:"string_replace"`
+	Arguments []string `json:"arguments,omitempty"`
+	InputPath *string `json:"input_path,omitempty"`
+	InputVariable *string `json:"input_variable,omitempty"`
+	OutputPath *string `json:"output_path,omitempty"`
+	OutputVariable *string `json:"output_variable,omitempty"`
+	StringReplace *StringReplace `json:"string_replace,omitempty"`
+	Type Type `json:"type"`
 }
 
 type StringReplace struct {

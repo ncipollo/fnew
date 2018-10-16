4235a029cd293558f881c8922b4c3f2d1289add5
Nick Cipollo <njc1115@gmail.com>
2018-10-16 09:05:21 -0400

    Add initial variables.

--- transform/transform.go
+++ transform/transform.go
@@ -15,16 +15,22 @@ type Transform interface {
 }
 
 type Options struct {
-	Arguments []string `json:"arguments,omitempty"`
-	InputPath string `json:"input_path,omitempty"`
-	InputVariable string `json:"input_variable,omitempty"`
-	OutputPath string `json:"output_path,omitempty"`
-	OutputVariable string `json:"output_variable,omitempty"`
-	StringReplace StringReplace `json:"string_replace,omitempty"`
-	Type Type `json:"type"`
+	Arguments      []string      `json:"arguments,omitempty"`
+	InputPath      string        `json:"input_path,omitempty"`
+	InputVariable  string        `json:"input_variable,omitempty"`
+	OutputPath     string        `json:"output_path,omitempty"`
+	OutputVariable string        `json:"output_variable,omitempty"`
+	StringReplace  StringReplace `json:"string_replace,omitempty"`
+	Type           Type          `json:"type"`
 }
 
 type StringReplace struct {
 	Old string `json:"old"`
 	New string `json:"new"`
+}
+
+type Variables map[string]string
+
+func NewVariables() Variables {
+	return make(Variables)
 }
